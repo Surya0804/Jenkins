480f2b78ba7193b3944b059fa196dd1a336a3467
Surya0804 <118931448+Surya0804@users.noreply.github.com>
2022-12-02 21:14:06 +0530

    Update Welcome.java

--- src/main/java/com/example/jenkinsProject/Code/Welcome.java
+++ src/main/java/com/example/jenkinsProject/Code/Welcome.java
@@ -5,6 +5,8 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To LTIMindtree");
+		System.out.println("Welcome Surya");
+
 
 	}
 
