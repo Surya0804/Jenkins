bf641ab996cd6203a473d74bfabafb9dc0c89c1d
Surya <Surya08042507@gmail.com>
2022-12-02 21:11:13 +0530

    File saved successfully

---
+++ src/main/java/com/example/jenkinsProject/Code/Welcome.java
@@ -0,0 +1,11 @@
+package com.example.jenkinsProject.Code;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To LTIMindtree");
+
+	}
+
+}
