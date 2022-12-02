c4f6f68e39627a98e820628c9193d1f8d5a2ffeb
Surya <Surya08042507@gmail.com>
2022-12-02 21:35:54 +0530

    Modified surya

--- src/main/java/com/example/jenkinsProject/Code/Welcome.java
+++ src/main/java/com/example/jenkinsProject/Code/Welcome.java
@@ -1,9 +1,15 @@
 package com.example.jenkinsProject.Code;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.jenkinsProject.JenkinsProjectApplication;
+
 public class Welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final Logger logger=LoggerFactory.getLogger(JenkinsProjectApplication.class);
+		logger.info("Built Successfully Team Surya");
 		System.out.println("Welcome To LTIMindtree");
 		System.out.println("Welcome Surya");
 
