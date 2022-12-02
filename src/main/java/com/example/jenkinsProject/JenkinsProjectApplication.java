b84b5df8d9df17ba94d28ea174d2279b94891c5d
Surya <Surya08042507@gmail.com>
2022-12-03 00:22:08 +0530

    modified surya

--- src/main/java/com/example/jenkinsProject/JenkinsProjectApplication.java
+++ src/main/java/com/example/jenkinsProject/JenkinsProjectApplication.java
@@ -2,16 +2,24 @@ package com.example.jenkinsProject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JenkinsProjectApplication {
+public class JenkinsProjectApplication implements CommandLineRunner {
 	private static final Logger logger=LoggerFactory.getLogger(JenkinsProjectApplication.class);
 
 	public static void main(String[] args) {
-		logger.info("Built Successfully Team Surya");
+		logger.info("Build started surya");
 		SpringApplication.run(JenkinsProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("Build ended surya");
+
 	}
 
 }
