fc103f450919975f45ae11c272291f14a0fa94b1
Surya <Surya08042507@gmail.com>
2022-12-02 22:10:57 +0530

    Modified surya

--- src/main/java/com/example/jenkinsProject/Code/Welcome.java
+++ src/main/java/com/example/jenkinsProject/Code/Welcome.java
@@ -1,15 +1,11 @@
 package com.example.jenkinsProject.Code;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.example.jenkinsProject.JenkinsProjectApplication;
 
 public class Welcome {
 
 	public static void main(String[] args) {
-		final Logger logger=LoggerFactory.getLogger(JenkinsProjectApplication.class);
-		logger.debug("Built Successfully Team Surya");
+		
 		System.out.println("Welcome To LTIMindtree");
 		System.out.println("Welcome Surya");
 
--- src/main/java/com/example/jenkinsProject/JenkinsProjectApplication.java
+++ src/main/java/com/example/jenkinsProject/JenkinsProjectApplication.java
@@ -1,12 +1,16 @@
 package com.example.jenkinsProject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsProjectApplication {
+	private static final Logger logger=LoggerFactory.getLogger(JenkinsProjectApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Built Successfully Team Surya");
 		SpringApplication.run(JenkinsProjectApplication.class, args);
 	}
 
